7b7b5f8efc168b958bfada674e19c12bd15aaf02
Pablo <pablojacobo1998@gmail.com>
2018-10-14 17:36:59 -0600

    h.redirect

--- server.js
+++ server.js
@@ -6,27 +6,37 @@ const server = hapi.server({
     host: 'localhost'                     //Escribiendo el core
 });
 
-
+//Configuraciones basicass de las rutas
 async function init() {
+    
+    //Devolviedo el GET Principal
     server.route({
         method: 'GET',
         path: '/',
         handler: (req, h)=>{
-            return 'Has entrado al get'
+            return h.response('Success').status(200);
+        }
+    });
+
+    //Ruta #2
+    server.route({
+        method: 'GET',
+        path: '/mi-github',
+        handler : (req, h)=>{
+            return h.redirect('https://github.com/p-jacobo2012240'); //Renderizando las vistas
         }
     });
     
     //Controlando posibles fallos no definidos
    try{
-    await server.start();
+        await server.start();
    }catch(err){
-       console.error('Error' + err);
-       process.exit(1);
+        console.error('Error' + err);
+        process.exit(1);
    }
    
    console.log(`
-    INFO  -> ${server.info.uri}
-   
+    INFO-SERVER  -> ${server.info.uri}
    `);
 }
 
