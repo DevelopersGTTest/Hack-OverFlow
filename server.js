52b37ecf1afd6ab1b11fbfa24deb6c92e04f142d
Pablo <pablojacobo1998@gmail.com>
2018-10-14 19:31:37 -0600

    Engines Hbs Renderizando views

--- server.js
+++ server.js
@@ -1,39 +1,66 @@
+//Core Packages del proyecto
 const hapi = require('hapi');
-
+const inert = require('inert'); //Puedo servir archivos o renderizar toda la vista
+const path = require('path');
+const handlebars = require('handlebars');
+const vision = require('vision');
 
 const server = hapi.server({
     port: process.env.PORT || 3000,
-    host: 'localhost'                     //Escribiendo el core
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: path.join(__dirname, 'public')
+        }
+    }                     //Escribiendo el core
 });
 
-//Configuraciones basicass de las rutas
-async function init() {
+
+async function init () {
+    try {
     
-    //Devolviedo el GET Principal
-    server.route({
+     //Permitiendo el acceso a servir el template   
+    await server.register(inert)
+    await server.register(vision)
+
+    //Implementando un objeto de configuracion
+    server.views({
+        engines: {
+            hbs: handlebars
+          },
+          relativeTo: __dirname,
+          path: 'views',
+          layout: true,
+          layoutPath: 'views'
+    })
+
+    //Mi home
+      server.route({
         method: 'GET',
         path: '/',
-        handler: (req, h)=>{
-            return h.response('Success').status(200);
+        handler: (req, h) => {
+          return h.view('index', {
+            titulo: 'Lord Hackobo'
+          });
         }
-    });
-
-    //Ruta #2
-    server.route({
+      })
+  
+      server.route({
         method: 'GET',
-        path: '/mi-github',
-        handler : (req, h)=>{
-            return h.redirect('https://github.com/p-jacobo2012240'); //Renderizando las vistas
+        path: '/{param*}',
+        handler: {
+          directory: {
+            path: '.',
+            index: ['index.html']
+          }
         }
-    });
-    
-    //Controlando posibles fallos no definidos
-   try{
-        await server.start();
-   }catch(err){
-        console.error('Error' + err);
-        process.exit(1);
-   }
+      })
+  
+      await server.start()
+    } catch (error) {
+      console.error(error)
+      process.exit(1)
+    }
    
    console.log(`
     INFO-SERVER  -> ${server.info.uri}
