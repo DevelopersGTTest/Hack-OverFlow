eee152d6c9a30c81b88029133641b14fe07654bd
unknown <pablojacobo1998@gmail.com>
2018-10-31 23:09:29 -0600

    Correct Answer Selected

--- models/questions.js
+++ models/questions.js
@@ -33,19 +33,19 @@ class Questions {
     return answers
   }
 
-  async setAnswerRight(questionId, answerId, user ){
+  async setAnswerRight (questionId, answerId, user) {
     const query = await this.collection.child(questionId).once('value')
     const question = query.val()
     const answers = question.answers
 
-    if(!user.email === user.owner.email){
-      return false //Para verfiicar que sea quien creo la pregunta
+    if (!user.email === question.owner.email) {
+      return false
     }
 
-    for( let key in answers ){
-      answers[key].correct = (key === answerId) //Recorriendo la respuesta correcta que estamos iterando
+    for (let key in answers) {
+      answers[key].correct = (key === answerId)
     }
-    
+
     const update = await this.collection.child(questionId).child('answers').update(answers)
     return update
   }
